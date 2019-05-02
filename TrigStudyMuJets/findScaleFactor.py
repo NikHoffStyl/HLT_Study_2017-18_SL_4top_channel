d9e0f7539d7acecfbb41cd3565b50cfe5413deb8
NikosStylianou <nikos.stylianou@bristol.ac.uk>
2019-05-02 13:45:41 +0100

    accept local files

--- TrigStudyMuJets/findScaleFactor.py
+++ TrigStudyMuJets/findScaleFactor.py
@@ -513,11 +513,11 @@ def main():
             h_dataSEls[hName] = h_dataSEls17C[hName]
         else:
             h_mcTTToSemiLeps[hName] = h_mcTTToSemiLeps17D[hName]
-            h_dataHTMHTs[hName] = ROOT.Add(h_dataHTMHTs17D[hName], h_dataHTMHTs17E[hName], 1, 1)
+            h_dataHTMHTs[hName].Add(h_dataHTMHTs17D[hName], h_dataHTMHTs17E[hName], 1, 1)
             h_dataHTMHTs[hName].Add(h_dataHTMHTs17F[hName])
-            h_dataSMus[hName] = ROOT.Add(h_dataSMus17D[hName], h_dataSMus17E[hName], 1, 1)
+            h_dataSMus[hName].Add(h_dataSMus17D[hName], h_dataSMus17E[hName], 1, 1)
             h_dataSMus[hName].Add(h_dataSMus17F[hName])
-            h_dataSEls[hName] = ROOT.Add(h_dataSEls17D[hName], h_dataSEls17E[hName], 1, 1)
+            h_dataSEls[hName].Add(h_dataSEls17D[hName], h_dataSEls17E[hName], 1, 1)
             h_dataSEls[hName].Add(h_dataSEls17F[hName])
 
         h_mcTTToSemiLeps[hName].SetTitle("Top-AntiTop MC")
