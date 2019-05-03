6d61849613d7a1a9eb91ad5e43a59927032b4e08
NikosStylianou <nikos.stylianou@bristol.ac.uk>
2019-05-03 01:01:24 +0100

    added electron jobs

--- TrigStudyElJets/run2017dataJob.sh
+++ TrigStudyElJets/run2017dataJob.sh
@@ -12,28 +12,28 @@ export X509_USER_PROXY=/user/$USER/x509up_u23075 # $(#id -u $USER)
 
 cd /user/nistylia/CMSSW_9_4_10/src/TopBrussels/RemoteWork/TrigStudyElJets
 
-#python runAnaTrigsElJets.py -f tt_semilep102_17B
-#python runAnaTrigsElJets.py -f tt_semilep102_17C
-#python runAnaTrigsElJets.py -f tt_semilep102_17DEF
+#python runAnaTrigsElJets.py -o _HTcut -f tt_semilep102_17B
+#python runAnaTrigsElJets.py -o _HTcut -f tt_semilep102_17C
+python runAnaTrigsElJets.py -o _HTcut -f tt_semilep102_17DEF
 
-#python runAnaTrigsElJets.py -f tttt102_17B
-#python runAnaTrigsElJets.py -f tttt102_17C
-#python runAnaTrigsElJets.py -f tttt102_17DEF
+#python runAnaTrigsElJets.py -o _HTcut -f tttt102_17B
+#python runAnaTrigsElJets.py -o _HTcut -f tttt102_17C
+#python runAnaTrigsElJets.py -o _HTcut -f tttt102_17DEF
 
-#python runAnaTrigsElJets.py -f dataHTMHT17B
-#python runAnaTrigsElJets.py -f dataHTMHT17C
-#python runAnaTrigsElJets.py -f dataHTMHT17D
-#python runAnaTrigsElJets.py -f dataHTMHT17E
-#python runAnaTrigsElJets.py -f dataHTMHT17F
+#python runAnaTrigsElJets.py -o _HTcut -f dataHTMHT17B
+#python runAnaTrigsElJets.py -o _HTcut -f dataHTMHT17C
+#python runAnaTrigsElJets.py -o _HTcut -f dataHTMHT17D
+#python runAnaTrigsElJets.py -o _HTcut -f dataHTMHT17E
+#python runAnaTrigsElJets.py -o _HTcut -f dataHTMHT17F
 #
-#python runAnaTrigsElJets.py -f dataSMu17B
-#python runAnaTrigsElJets.py -f dataSMu17C
-#python runAnaTrigsElJets.py -f dataSMu17D
-#python runAnaTrigsElJets.py -f dataSMu17E
-#python runAnaTrigsElJets.py -f dataSMu17F
+#python runAnaTrigsElJets.py -o _HTcut -f dataSMu17B
+#python runAnaTrigsElJets.py -o _HTcut -f dataSMu17C
+#python runAnaTrigsElJets.py -o _HTcut -f dataSMu17D
+#python runAnaTrigsElJets.py -o _HTcut -f dataSMu17E
+#python runAnaTrigsElJets.py -o _HTcut -f dataSMu17F
 #
-#python runAnaTrigsElJets.py -f dataSEl17B
-#python runAnaTrigsElJets.py -f dataSEl17C
-#python runAnaTrigsElJets.py -f dataSEl17D
-#python runAnaTrigsElJets.py -f dataSEl17E
-python runAnaTrigsElJets.py -f dataSEl17F
+#python runAnaTrigsElJets.py -o _HTcut -f dataSEl17B
+#python runAnaTrigsElJets.py -o _HTcut -f dataSEl17C
+#python runAnaTrigsElJets.py -o _HTcut -f dataSEl17D
+#python runAnaTrigsElJets.py -o _HTcut -f dataSEl17E
+#python runAnaTrigsElJets.py -o _HTcut -f dataSEl17F
